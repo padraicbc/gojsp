1da22db5438f35b8df537b86ec50d5c7f70f1c75
padraicbc <padraicbc@users.noreply.github.com>
2021-04-27 21:29:47 +0100

    all todo

---
+++ vast/todo.go
@@ -0,0 +1,114 @@
+package vast
+
+import "github.com/padraicbc/gojsp/base"
+
+// todo: implement
+
+func (v *Visitor) VisitCaseBlock(ctx *base.CaseBlockContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+func (v *Visitor) VisitCaseClauses(ctx *base.CaseClausesContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitCaseClause(ctx *base.CaseClauseContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitDefaultClause(ctx *base.DefaultClauseContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitThrowStatement(ctx *base.ThrowStatementContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitTryStatement(ctx *base.TryStatementContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitCatchProduction(ctx *base.CatchProductionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitFinallyProduction(ctx *base.FinallyProductionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitDebuggerStatement(ctx *base.DebuggerStatementContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+func (v *Visitor) VisitTemplateStringExpression(ctx *base.TemplateStringExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+func (v *Visitor) VisitPreIncrementExpression(ctx *base.PreIncrementExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+func (v *Visitor) VisitMetaExpression(ctx *base.MetaExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitNotExpression(ctx *base.NotExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitPreDecreaseExpression(ctx *base.PreDecreaseExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitThisExpression(ctx *base.ThisExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+func (v *Visitor) VisitUnaryMinusExpression(ctx *base.UnaryMinusExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitPostDecreaseExpression(ctx *base.PostDecreaseExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitTypeofExpression(ctx *base.TypeofExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+func (v *Visitor) VisitUnaryPlusExpression(ctx *base.UnaryPlusExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitDeleteExpression(ctx *base.DeleteExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitSuperExpression(ctx *base.SuperExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+func (v *Visitor) VisitPostIncrementExpression(ctx *base.PostIncrementExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitYieldExpression(ctx *base.YieldExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitBitNotExpression(ctx *base.BitNotExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitNewExpression(ctx *base.NewExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitArrayLiteralExpression(ctx *base.ArrayLiteralExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitClassExpression(ctx *base.ClassExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitMemberIndexExpression(ctx *base.MemberIndexExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *Visitor) VisitVoidExpression(ctx *base.VoidExpressionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
